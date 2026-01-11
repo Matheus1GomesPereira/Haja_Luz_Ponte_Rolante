2f76c35c89a9c0fbbed212cc735e547c02ed395d
Matheus Gomes Pereira <89620294+Matheus1GomesPereira@users.noreply.github.com>
2026-01-10 22:44:31 -0300

    commit
    ajustando informações nos slides e trocando a logo ao rolar a pagina

--- menu.js
+++ menu.js
@@ -1,9 +1,17 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector(".header_section");
+  const logoImg = document.getElementById("logo-img");
+
+  // Caminhos das logos
+  const logoTopo = "images/logo.webp";     // logo padrão
+  const logoScroll = "images/logo1.webp";  // logo ao rolar
 
   if (window.scrollY > 10) {
     header.classList.add("scrolled");
+    logoImg.src = logoScroll;
   } else {
     header.classList.remove("scrolled");
+    logoImg.src = logoTopo;
   }
 });
+
